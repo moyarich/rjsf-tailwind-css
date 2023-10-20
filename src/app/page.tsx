8c222e0f8fde9018911f33112dd2a711fc4931d3
moyarich <moyarich@hotmail.com>
2023-10-20 04:48:44 -0500

    formatted spaces in files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,109 +1,109 @@
-'use client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+"use client";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { NextPage } from "next";
 import { useParams } from "next/navigation";
 
-import { ErrorSchema, RJSFSchema, UiSchema } from '@rjsf/utils';
-import { IChangeEvent } from '@rjsf/core';
+import { ErrorSchema, RJSFSchema, UiSchema } from "@rjsf/utils";
+import { IChangeEvent } from "@rjsf/core";
 
-import validator from '@rjsf/validator-ajv8';
+import validator from "@rjsf/validator-ajv8";
 
-import Editors from '@/components/form/editor/editors-dock';
+import Editors from "@/components/form/editor/editors-dock";
 
 // Import your schema and uiSchema data here
-import * as homeSchema from '@/schemas/home';
-import * as menuSchema from '@/schemas/menu';
-import * as randomSchema from '@/schemas/random';
-import * as videoSchema from '@/schemas/video';
-import * as testingSchema from '@/schemas/_testing';
+import * as homeSchema from "@/schemas/home";
+import * as menuSchema from "@/schemas/menu";
+import * as randomSchema from "@/schemas/random";
+import * as videoSchema from "@/schemas/video";
+import * as testingSchema from "@/schemas/_testing";
 
 const defaultSchemaData = {
-  Home: homeSchema,
-  Menu: menuSchema,
-  Random: randomSchema,
-  Video: videoSchema,
-  Testing: testingSchema,
-  //'Diseases': diseasesSchema,
+    Home: homeSchema,
+    Menu: menuSchema,
+    Random: randomSchema,
+    Video: videoSchema,
+    Testing: testingSchema,
+    //'Diseases': diseasesSchema,
 };
 
-
-import { SchemaSelector } from '@/components/schema-selector';
-
+import { SchemaSelector } from "@/components/schema-selector";
 
 const Page: NextPage = () => {
-  const params = useParams();
+    const params = useParams();
 
-  const [selectedSchema, setSelectedSchema] = useState('Home');
+    const [selectedSchema, setSelectedSchema] = useState("Home");
 
-  const [schema, setSchema] = useState<RJSFSchema>( {});
-  const [uiSchema, setUiSchema] = useState<UiSchema>({});
-  const [formData, setFormData] = useState<any>({});
+    const [schema, setSchema] = useState<RJSFSchema>({});
+    const [uiSchema, setUiSchema] = useState<UiSchema>({});
+    const [formData, setFormData] = useState<any>({});
 
-  const [extraErrors, setExtraErrors] = useState<ErrorSchema | undefined>();
+    const [extraErrors, setExtraErrors] = useState<ErrorSchema | undefined>();
 
-  function handleSchemaSelectChange(selected: string): void {
-    setSelectedSchema(selected);
-  }
+    function handleSchemaSelectChange(selected: string): void {
+        setSelectedSchema(selected);
+    }
 
-  useEffect(() => {
-    const {
-        schema: s = {},
-        uiSchema: uiS = {},
-        formData: fd = {},
-    } = defaultSchemaData?.[selectedSchema as keyof typeof defaultSchemaData]  ?? {};
+    useEffect(() => {
+        const {
+            schema: s = {},
+            uiSchema: uiS = {},
+            formData: fd = {},
+        } = defaultSchemaData?.[selectedSchema as keyof typeof defaultSchemaData] ?? {};
 
-    setSchema(s as RJSFSchema);
-    setUiSchema(uiS as RJSFSchema);
-    setFormData(fd as FormData);
-}, [selectedSchema]);
+        setSchema(s as RJSFSchema);
+        setUiSchema(uiS as RJSFSchema);
+        setFormData(fd as FormData);
+    }, [selectedSchema]);
 
-
-  const handleFormDataChange = (form: IChangeEvent<unknown>, id?: string) => {
-    console.log(id, form.formData);
-};
+    const handleFormDataChange = (form: IChangeEvent<unknown>, id?: string) => {
+        console.log(id, form.formData);
+    };
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const handleFormDataSubmit = (form: IChangeEvent<unknown>, _event: unknown) => {
-      setFormData(form.formData);
-  };
+        setFormData(form.formData);
+    };
 
-  const handleTemplateSave = (
-    schema: RJSFSchema,
-    uiSchema?: UiSchema,
-    formData?: unknown,
-    extraErrors?: ErrorSchema
-) => {
-  console.log("-->handleTemplateSave schema-->",schema)
-  console.log("-->handleTemplateSave uiSchema-->",uiSchema)
-  console.log("-->handleTemplateSave formData-->",formData)
-  console.log("-->handleTemplateSave extraErrors-->",extraErrors)
-}
+    const handleTemplateSave = (
+        schema: RJSFSchema,
+        uiSchema?: UiSchema,
+        formData?: unknown,
+        extraErrors?: ErrorSchema
+    ) => {
+        console.log("-->handleTemplateSave schema-->", schema);
+        console.log("-->handleTemplateSave uiSchema-->", uiSchema);
+        console.log("-->handleTemplateSave formData-->", formData);
+        console.log("-->handleTemplateSave extraErrors-->", extraErrors);
+    };
 
+    return (
+        <div className="container">
+            <SchemaSelector
+                selectedSchema={selectedSchema}
+                handleSchemaSelectChange={handleSchemaSelectChange}
+                schemaData={defaultSchemaData}
+            />
 
-  return (
-    <div className="container">
-     <SchemaSelector selectedSchema={selectedSchema} handleSchemaSelectChange={handleSchemaSelectChange } schemaData={defaultSchemaData} />
+            {
+                <Editors
+                    schema={schema}
+                    uiSchema={uiSchema}
+                    formData={formData}
+                    validator={validator}
+                    setFormData={setFormData}
+                    setSchema={setSchema}
+                    setUiSchema={setUiSchema}
+                    extraErrors={extraErrors}
+                    setExtraErrors={setExtraErrors}
+                    onFormDataChange={handleFormDataChange}
+                    onFormDataSubmit={handleFormDataSubmit}
+                    onTemplateSave={handleTemplateSave}
+                />
+            }
+        </div>
+    );
+};
 
-      {
-        <Editors
-          schema={schema}
-          uiSchema={uiSchema}
-          formData={formData}
-          validator={validator}
-          setFormData={setFormData}
-          setSchema={setSchema}
-          setUiSchema={setUiSchema}
-          extraErrors={extraErrors}
-          setExtraErrors={setExtraErrors}
-          onFormDataChange={handleFormDataChange}
-          onFormDataSubmit={handleFormDataSubmit}
-          onTemplateSave={handleTemplateSave}
-        />
-      }
-    </div>
-  );
-}
-
-export default Page
+export default Page;
--- src/components/form/editor/editors.tsx
+++ src/components/form/editor/editors.tsx
@@ -1,191 +1,194 @@
+import React, { useCallback, useState } from "react";
 
-import React, { useCallback, useState } from 'react';
+import { ErrorSchema, RJSFSchema, UiSchema, ValidatorType, FormContextType } from "@rjsf/utils";
+import { IChangeEvent, FormProps } from "@rjsf/core";
 
-import { ErrorSchema, RJSFSchema, UiSchema, ValidatorType,FormContextType } from '@rjsf/utils';
-import { IChangeEvent, FormProps } from '@rjsf/core';
+import isEqualWith from "lodash/isEqualWith";
 
-import isEqualWith from 'lodash/isEqualWith';
-
-
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import FormBuilderGuiEditor from "./form-builder-gui-editor";
-import Editor from './editor';
-import ThemeForm from '../theme';
+import Editor from "./editor";
+import ThemeForm from "../theme";
 
 const toJson = (val: unknown) => JSON.stringify(val ?? {}, null, "\t");
 
 interface EditorsProps {
-  editorTitle?: string;
-  schema: RJSFSchema;
-  setSchema: React.Dispatch<React.SetStateAction<RJSFSchema>>;
-  uiSchema: UiSchema;
-  setUiSchema: React.Dispatch<React.SetStateAction<UiSchema>>;
-  formData: unknown;
-  setFormData: React.Dispatch<React.SetStateAction<unknown>>;
-  validator: ValidatorType;
-  extraErrors?: ErrorSchema;
-  setExtraErrors?: React.Dispatch<React.SetStateAction<ErrorSchema | undefined>>;
-  onFormDataChange?: (form: IChangeEvent<unknown, RJSFSchema, FormContextType>, id?: string) => void;
-  onFormDataSubmit?: (
-    form: IChangeEvent<unknown, RJSFSchema, FormContextType>,
-    event: React.FormEvent<unknown>
-  ) => void;
-  onTemplateSave?:(schema : RJSFSchema,uiSchema?: UiSchema,formData?:unknown, extraErrors?:ErrorSchema ) => void
+    editorTitle?: string;
+    schema: RJSFSchema;
+    setSchema: React.Dispatch<React.SetStateAction<RJSFSchema>>;
+    uiSchema: UiSchema;
+    setUiSchema: React.Dispatch<React.SetStateAction<UiSchema>>;
+    formData: unknown;
+    setFormData: React.Dispatch<React.SetStateAction<unknown>>;
+    validator: ValidatorType;
+    extraErrors?: ErrorSchema;
+    setExtraErrors?: React.Dispatch<React.SetStateAction<ErrorSchema | undefined>>;
+    onFormDataChange?: (
+        form: IChangeEvent<unknown, RJSFSchema, FormContextType>,
+        id?: string
+    ) => void;
+    onFormDataSubmit?: (
+        form: IChangeEvent<unknown, RJSFSchema, FormContextType>,
+        event: React.FormEvent<unknown>
+    ) => void;
+    onTemplateSave?: (
+        schema: RJSFSchema,
+        uiSchema?: UiSchema,
+        formData?: unknown,
+        extraErrors?: ErrorSchema
+    ) => void;
 }
 
 export default function Editors({
-  editorTitle,
-  extraErrors,
-  setExtraErrors,
-  setFormData,
-  schema,
-  setSchema,
-  uiSchema,
-  setUiSchema,
-  formData,
-  validator,
-  onFormDataChange,
-  onFormDataSubmit,
-  onTemplateSave,
+    editorTitle,
+    extraErrors,
+    setExtraErrors,
+    setFormData,
+    schema,
+    setSchema,
+    uiSchema,
+    setUiSchema,
+    formData,
+    validator,
+    onFormDataChange,
+    onFormDataSubmit,
+    onTemplateSave,
 }: EditorsProps) {
- 
+    const _onTemplateSave = () => {
+        onTemplateSave && onTemplateSave(schema, uiSchema, formData, extraErrors);
+    };
 
-  const _onTemplateSave = () => {
-    onTemplateSave && onTemplateSave(schema ,uiSchema,formData, extraErrors)
+    //-------------------
+    //-------------------
+    /**
+     * FormPreview
+     */
+    const _onFormDataChange = (form: IChangeEvent<unknown>, id: string | undefined) => {
+        onFormDataChange && onFormDataChange(form, id);
+    };
 
-  }
+    const _onFormDataSubmit = (
+        form: IChangeEvent<unknown, RJSFSchema, FormContextType>,
+        event: React.FormEvent<unknown>
+    ) => {
+        onFormDataSubmit && onFormDataSubmit(form, event);
+    };
 
-  //-------------------
-  //-------------------
-  /**
-   * FormPreview
-   */
-  const _onFormDataChange = (form: IChangeEvent<unknown>, id: string | undefined) => {
-      onFormDataChange && onFormDataChange(form, id);
-    }
+    const FormPreview = () => {
+        return (
+            <ThemeForm
+                schema={schema}
+                uiSchema={uiSchema}
+                formData={formData}
+                onChange={_onFormDataChange}
+                onSubmit={_onFormDataSubmit}
+                validator={validator}
+            />
+        );
+    };
 
+    //-------------------
+    //-------------------
+    /**
+     * Schema
+     */
 
-  const _onFormDataSubmit = 
-    (form: IChangeEvent<unknown, RJSFSchema, FormContextType>, event: React.FormEvent<unknown>) => {
-      onFormDataSubmit && onFormDataSubmit(form, event);
-    }
+    const SchemaEditor = () => {
+        return (
+            <Editor
+                title="JSONSchema"
+                code={toJson(schema)}
+                onSave={(code: string) => {
+                    const codeObject = JSON.parse(code);
+                    setSchema(codeObject);
+                }}
+            />
+        );
+    };
 
+    //-------------------
+    //-------------------
+    /**
+     * UI Schema
+     */
 
-  const FormPreview = () => {
+    const UISchemaEditor = () => {
+        return (
+            <Editor
+                title="UISchema"
+                code={toJson(uiSchema)}
+                onSave={(code: string) => {
+                    const codeObject = JSON.parse(code);
+                    setUiSchema(codeObject);
+                }}
+            />
+        );
+    };
+
+    //-------------------
+    //-------------------
+    /**
+     * FormData
+     */
+
+    const onFormDataEdited = (newFormData: any) => {
+        if (
+            !isEqualWith(newFormData, formData, (newValue, oldValue) => {
+                // The editor uses JSON.stringify to remove undefined values. So, compare the values and use JSON.stringify to check if the trimmed formData is identical to the untrimmed formData.
+                return JSON.stringify(oldValue) === JSON.stringify(newValue);
+            })
+        ) {
+            setFormData(newFormData);
+        }
+    };
+
+    const FormDataEditor = () => {
+        return (
+            <Editor
+                title="formData"
+                code={toJson(formData)}
+                onSave={(code: string) => {
+                    const codeObject = JSON.parse(code);
+                    onFormDataEdited(codeObject);
+                }}
+            />
+        );
+    };
+
+    //-------------------
+    //-------------------
+    /**
+     * Extra Errors
+     */
+
+    const onExtraErrorsEdited = (newExtraErrors?: ErrorSchema) => {
+        if (newExtraErrors) {
+            setExtraErrors && setExtraErrors(newExtraErrors);
+        }
+    };
+
+    const ExtraErrorsEditorEditor = () => {
+        return (
+            <Editor
+                title="extraErrors"
+                code={toJson(extraErrors || {})}
+                onSave={(code: string) => {
+                    const codeObject = JSON.parse(code);
+                    onExtraErrorsEdited(codeObject);
+                }}
+            />
+        );
+    };
+
     return (
-      <ThemeForm
-        schema={schema}
-        uiSchema={uiSchema}
-        formData={formData}
-        onChange={_onFormDataChange}
-        onSubmit={_onFormDataSubmit}
-        validator={validator}
-      />
-    );
-  };
-
-  //-------------------
-  //-------------------
-  /**
-   * Schema
-   */
-
-  const SchemaEditor = () => {
-    return (
-      <Editor
-        title="JSONSchema"
-        code={toJson(schema)}
-        onSave={(code: string) => {
-          const codeObject = JSON.parse(code);
-          setSchema(codeObject);
-        }}
-      />
-    );
-  };
-
-  //-------------------
-  //-------------------
-  /**
-   * UI Schema
-   */
-
-
-  const UISchemaEditor = () => {
-    return (
-      <Editor
-        title="UISchema"
-        code={toJson(uiSchema)}
-        onSave={(code: string) => {
-          const codeObject = JSON.parse(code);
-          setUiSchema(codeObject);
-        }}
-      />
-    );
-  };
-
-  //-------------------
-  //-------------------
-  /**
-   * FormData
-   */
-
-  const onFormDataEdited =
-    (newFormData: any) => {
-      if (
-        !isEqualWith(newFormData, formData, (newValue, oldValue) => {
-          // The editor uses JSON.stringify to remove undefined values. So, compare the values and use JSON.stringify to check if the trimmed formData is identical to the untrimmed formData.
-          return JSON.stringify(oldValue) === JSON.stringify(newValue);
-        })
-      ) {
-        setFormData(newFormData);
-      }
-    }
-
-  const FormDataEditor = () => {
-    return (
-      <Editor
-        title="formData"
-        code={toJson(formData)}
-        onSave={(code: string) => {
-          const codeObject = JSON.parse(code);
-          onFormDataEdited(codeObject);
-        }}
-      />
-    );
-  };
-
-  //-------------------
-  //-------------------
-  /**
-   * Extra Errors
-   */
-
-  const onExtraErrorsEdited = (newExtraErrors?: ErrorSchema) => {
-      if(newExtraErrors){
-        setExtraErrors && setExtraErrors(newExtraErrors);
-      }
-    }
-
-  const ExtraErrorsEditorEditor = () => {
-    return (
-      <Editor
-        title="extraErrors"
-        code={toJson(extraErrors || {})}
-        onSave={(code: string) => {
-          const codeObject = JSON.parse(code);
-          onExtraErrorsEdited(codeObject);
-        }}
-      />
-    );
-  };
-
-  return (
-    <div className="mb-4">
-                <div className="header flex pt-2 items-center">
-                {editorTitle && (<div className="p-2 gap-1 inline-flex items-center">
-                    <span className="text-lg">{editorTitle}</span>
-                </div>)}
+        <div className="mb-4">
+            <div className="header flex pt-2 items-center">
+                {editorTitle && (
+                    <div className="p-2 gap-1 inline-flex items-center">
+                        <span className="text-lg">{editorTitle}</span>
+                    </div>
+                )}
                 <div className="action flex flex-1 items-center justify-end">
                     <button
                         className="bg-primary text-primary-foreground shadow hover:bg-primary/90 py-2 rounded-tl-md rounded-tr-md px-8"
@@ -196,64 +199,64 @@ export default function Editors({
                 </div>
             </div>
             <div className="p-1 rounded-tl-md rounded-bl-md rounded-br-md border-4  border-primary border-opacity-10">
-      <Tabs defaultValue="source">
-        <TabsList className="w-full justify-start inline-flex h-9 items-center rounded-lg bg-muted p-1 text-muted-foreground">
-          <TabsTrigger value="source">Source</TabsTrigger>
-          <TabsTrigger value="formDesigner">Form Designer</TabsTrigger>
-        </TabsList>
-        <div className="flex gap-[15px] mb-6">
-          <div className="flex-1">
-            <TabsContent value="source">
-              <Tabs defaultValue="schema">
-                <TabsList className="h-[25px]">
-                  <TabsTrigger value="schema">Schema</TabsTrigger>
-                  <TabsTrigger value="uiSchema">UiSchema</TabsTrigger>
-                  <TabsTrigger value="formData">FormData</TabsTrigger>
+                <Tabs defaultValue="source">
+                    <TabsList className="w-full justify-start inline-flex h-9 items-center rounded-lg bg-muted p-1 text-muted-foreground">
+                        <TabsTrigger value="source">Source</TabsTrigger>
+                        <TabsTrigger value="formDesigner">Form Designer</TabsTrigger>
+                    </TabsList>
+                    <div className="flex gap-[15px] mb-6">
+                        <div className="flex-1">
+                            <TabsContent value="source">
+                                <Tabs defaultValue="schema">
+                                    <TabsList className="h-[25px]">
+                                        <TabsTrigger value="schema">Schema</TabsTrigger>
+                                        <TabsTrigger value="uiSchema">UiSchema</TabsTrigger>
+                                        <TabsTrigger value="formData">FormData</TabsTrigger>
 
-                  {extraErrors && (
-                    <TabsTrigger value="extraErrors">FormData</TabsTrigger>
-                  )}
-                </TabsList>
-                <div className="nested-tab-contents mt-3 h-[500px]  border-1 pb-2 overflow-hidden rounded-md resize">
-                  <TabsContent value="schema" className="h-full">
-                    <SchemaEditor />
-                  </TabsContent>
+                                        {extraErrors && (
+                                            <TabsTrigger value="extraErrors">FormData</TabsTrigger>
+                                        )}
+                                    </TabsList>
+                                    <div className="nested-tab-contents mt-3 h-[500px]  border-1 pb-2 overflow-hidden rounded-md resize">
+                                        <TabsContent value="schema" className="h-full">
+                                            <SchemaEditor />
+                                        </TabsContent>
 
-                  <TabsContent value="uiSchema" className="h-full">
-                    <UISchemaEditor />
-                  </TabsContent>
+                                        <TabsContent value="uiSchema" className="h-full">
+                                            <UISchemaEditor />
+                                        </TabsContent>
 
-                  <TabsContent value="formData" className="h-full">
-                    <FormDataEditor />
-                  </TabsContent>
+                                        <TabsContent value="formData" className="h-full">
+                                            <FormDataEditor />
+                                        </TabsContent>
 
-                  {extraErrors && (
-                    <TabsContent value="extraErrors" className="h-full">
-                      <ExtraErrorsEditorEditor />
-                    </TabsContent>
-                  )}
-                </div>
-              </Tabs>
-            </TabsContent>
-            <TabsContent value="formDesigner">
-              <div className="flex-1 mt-[49px]">
-              <FormBuilderGuiEditor
-      schema={toJson(schema)}
-      uiSchema={toJson(uiSchema)}
-        onChange={(newSchema: string, newUiSchema: string) => {
-          setSchema(JSON.parse(newSchema));
-          setUiSchema(JSON.parse(newUiSchema));
-        }}
-      />
-              </div>
-            </TabsContent>
-          </div>
-          <div className="mt-[49px] flex-1 border rounded-md overflow-hidden resize py-4 px-6 h-full">
-            <FormPreview />
-          </div>
+                                        {extraErrors && (
+                                            <TabsContent value="extraErrors" className="h-full">
+                                                <ExtraErrorsEditorEditor />
+                                            </TabsContent>
+                                        )}
+                                    </div>
+                                </Tabs>
+                            </TabsContent>
+                            <TabsContent value="formDesigner">
+                                <div className="flex-1 mt-[49px]">
+                                    <FormBuilderGuiEditor
+                                        schema={toJson(schema)}
+                                        uiSchema={toJson(uiSchema)}
+                                        onChange={(newSchema: string, newUiSchema: string) => {
+                                            setSchema(JSON.parse(newSchema));
+                                            setUiSchema(JSON.parse(newUiSchema));
+                                        }}
+                                    />
+                                </div>
+                            </TabsContent>
+                        </div>
+                        <div className="mt-[49px] flex-1 border rounded-md overflow-hidden resize py-4 px-6 h-full">
+                            <FormPreview />
+                        </div>
+                    </div>
+                </Tabs>
+            </div>
         </div>
-      </Tabs>
-    </div>
-    </div>
-  );
+    );
 }
