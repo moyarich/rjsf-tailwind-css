3aa0ea9297e8bfb23d000b838b1cc7539303f6fa
moyarich <moyarich@hotmail.com>
2023-10-23 11:29:12 -0500

    remove comments

--- src/components/schema-selector.tsx
+++ src/components/schema-selector.tsx
@@ -1,19 +1,3 @@
-/*
- *   Copyright (c) 2023 Amazon.com, Inc. and its affiliates.
- *   All rights reserved.
- *
- *   Licensed under the Amazon Software License (the "License").
- *   You may not use this file except in compliance with the License.
- *   A copy of the License is located at
- *
- *   http://aws.amazon.com/asl/
- *
- *   or in the "license" file accompanying this file. This file is distributed
- *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- *   express or implied. See the License for the specific language governing
- *   permissions and limitations under the License.
- */
-
 import {
     Select,
     SelectTrigger,
