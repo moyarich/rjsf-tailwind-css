bd812405284e0c69b0d882be074d6a3b3d1dd0f9
moyarich <moyarich@hotmail.com>
2023-10-27 16:16:04 -0500

    fixed the menu schema error

--- src/schemas/menu.ts
+++ src/schemas/menu.ts
@@ -1,63 +1,66 @@
 export const schema = {
-  title: 'Main Menu',
-  description: 'The main menu for categories',
-  type: 'object',
-  items: {
-    type: 'object',
+    title: "Main Menu",
+    description: "The main menu for categories",
+    type: "object",
     properties: {
-      id: {
-        type: 'string',
-        title: 'ID',
-      },
-      title: {
-        type: 'string',
-        title: 'Title',
-      },
-      url: {
-        type: 'string',
-        title: 'URL',
-      },
-      type: {
-        type: 'string',
-        title: 'Type',
-      },
-      dropdownItems: {
-        type: 'array',
+        type: "array",
         items: {
-          type: 'object',
-          properties: {
-            id: {
-              type: 'string',
-              title: 'ID',
+            type: "object",
+            properties: {
+                id: {
+                    type: "string",
+                    title: "ID",
+                },
+                title: {
+                    type: "string",
+                    title: "Title",
+                },
+                url: {
+                    type: "string",
+                    title: "URL",
+                },
+                type: {
+                    type: "string",
+                    title: "Type",
+                },
+                dropdownItems: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        properties: {
+                            id: {
+                                type: "string",
+                                title: "ID",
+                            },
+                            title: {
+                                type: "string",
+                                title: "Title",
+                            },
+                            url: {
+                                type: "string",
+                                title: "URL",
+                            },
+                            type: {
+                                type: "string",
+                                title: "Type",
+                            },
+                        },
+                    },
+                },
             },
-            title: {
-              type: 'string',
-              title: 'Title',
-            },
-            url: {
-              type: 'string',
-              title: 'URL',
-            },
-            type: {
-              type: 'string',
-              title: 'Type',
-            },
-          },
         },
-      },
     },
-  },
 };
 export const uiSchema = {
-  // Customize the UI schema as needed to control the form's appearance
+    // Customize the UI schema as needed to control the form's appearance
 };
 
 export const formData = {};
 
 const sample = {
-  schema,
-  uiSchema,
-  formData
-}
+    schema,
+    uiSchema,
+    formData,
+};
 
-export default sample
+export default sample;
